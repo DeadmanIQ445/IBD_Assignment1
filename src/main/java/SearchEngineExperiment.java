5d82257613ade13a14be3c40dee10c3069a52342
alex <gruk98@mail.ru>
2019-10-08 21:42:00 +0300

    Java Search Engine Macket

---
+++ src/main/java/SearchEngineExperiment.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SearchEngineExperiment {
+
+    public static void main(String[] args) throws IOException {
+        Map <Integer, Integer> documentFrequency= new HashMap<Integer, Integer>();
+        String searchWord = "and";
+        char[] searchWordToChar = searchWord.toCharArray();
+
+        String url = new File("").getAbsolutePath();
+        String searchBase = url + "/outputT/part-r-00000";
+        File file = new File(searchBase);
+
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = reader.readLine();
+
+        Boolean found;
+        while (line != null) {
+            line = reader.readLine();
+
+            if (line != null) {
+                char[] lineToChar = line.toCharArray();
+
+                found = true;
+                for (int i = 0; i < searchWord.length(); i++) {
+                    if (searchWordToChar[i] != lineToChar[i])
+                        found = false;
+                }
+                if (lineToChar[searchWord.length()] != ' ' && lineToChar[searchWord.length()] != '\t')
+                    found = false;
+
+                if (found)
+                    System.out.println(line);
+            }
+        }
+    }
+}
